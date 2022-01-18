412a73eb67f6b027c1adbb6019c42bc27e77757e
brisaurora <92859425+brisaurora@users.noreply.github.com>
2022-01-18 18:45:44 +0800

    CSAPP

---
+++ CSAPP/chapter2/fprintf or sprintf/cmake-build-debug/CMakeFiles/fprintf_or_sprintf.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/fprintf_or_sprintf.dir/main.c.obj"
+  "fprintf_or_sprintf.exe"
+  "fprintf_or_sprintf.exe.manifest"
+  "fprintf_or_sprintf.pdb"
+  "libfprintf_or_sprintf.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/fprintf_or_sprintf.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ CSAPP/chapter2/fprintf or sprintf/main.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main() {
+    long a;
+    off64_t c;
+    printf("%lld %lld!\n",sizeof (a),sizeof (c));
+    return 0;
+}
