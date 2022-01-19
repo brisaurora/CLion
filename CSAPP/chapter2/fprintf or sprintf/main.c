25527768ae7d15257044fad61a1c32725cca2314
brisaurora <92859425+brisaurora@users.noreply.github.com>
2022-01-19 15:42:52 +0800

    。

--- CSAPP/chapter2/fprintf or sprintf/main.c
+++ CSAPP/chapter2/fprintf or sprintf/main.c
@@ -1,8 +1,46 @@
 #include <stdio.h>
-
+#include <math.h>
+void test_address(void *x);
 int main() {
     long a;
     off64_t c;
     printf("%lld %lld!\n",sizeof (a),sizeof (c));
+    //fprintf
+    FILE * fp;
+    fp = fopen ("file.txt", "w+");
+    fprintf(fp, "%s %s %s %d", "We", "are", "in", 2014);
+    fclose(fp);
+
+    char d;
+    fp = fopen("file.txt","r");
+    while(1)
+    {
+        d = (char)fgetc(fp);
+        if( feof(fp) )
+        {
+            break ;
+        }
+        printf("%c", d);
+    }
+    fclose(fp);
+    //sprintf
+    char str[80];
+    float k=1.3f;
+    k=powf(k,2);
+    sprintf(str, "\nPi 的值 = %f %f", M_PI,k);
+    puts(str);
+    //x,&x
+    char *x;
+    d='2';
+    x=&d;
+    test_address(x);
     return 0;
+}
+void test_address(void *x)
+{
+
+    char *t;
+    t=(char *)x;
+    printf("%p %p %c",x,&x,*t);
+
 }
